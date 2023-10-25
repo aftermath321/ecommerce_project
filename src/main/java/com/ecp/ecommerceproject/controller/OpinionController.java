bcda0eafe5247c754d8c39c7da85f262ae700ab3
Michał Kiełbasa <70415923+aftermath321@users.noreply.github.com>
2023-10-25 16:27:40 +0200

    Added CORS for Controllers.

--- src/main/java/com/ecp/ecommerceproject/controller/OpinionController.java
+++ src/main/java/com/ecp/ecommerceproject/controller/OpinionController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin
 public class OpinionController {
 
     final OpinionService opinionService;
--- src/main/java/com/ecp/ecommerceproject/controller/UserController.java
+++ src/main/java/com/ecp/ecommerceproject/controller/UserController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin
 public class UserController {
 
     final UserService userService;
