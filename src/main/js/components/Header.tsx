199d26a429f8c36af7930eca4d0f1a6a7a64aac1
Michał Kiełbasa <70415923+aftermath321@users.noreply.github.com>
2023-10-30 18:27:39 +0100

    Simple_header

--- src/main/js/components/Header.tsx
+++ src/main/js/components/Header.tsx
@@ -4,12 +4,35 @@ const Header = () => {
   return (
     <div className="sticky top-0 w-full bg-slate-400 h-[100px] flex flex-row justify-between p-4">
       <div className="left-0 flex flex-row ">
-        <div className="p-4">Logo</div>
-        <div className="p-4">Categories</div>
+        <div className="p-4 font-extrabold text-white text-4xl">H</div>
+        <div className="p-4 font-extrabold text-white text-2xl">
+          HomeOFFFICE
+        </div>
+        <div className="p-4 flex">
+          <ul className="flex flex-row gap-4">
+            <li className="text-white self-center">Our locations</li>
+            <li className="text-white self-center">Store</li>
+            <li className="text-white self-center">Office</li>
+            <li className="text-white self-center">Home Office</li>
+          </ul>
+        </div>
       </div>
-      <div className="right-0">
-        
-
+      <div className="right-0 flex flex-row">
+        <ul className="flex flex-row gap-4">
+          <li className="text-white self-center">About</li>
+          <li className="text-white self-center">Contact Us</li>
+          <li className="text-white self-center">
+            <div className="flex gap-2">
+              <input
+                placeholder="Search products"
+                className="p-1 rounded-md"
+              ></input>
+              <button className="bg-yellow-300 px-3 rounded-md text-black font-bold">&gt;</button>
+            </div>
+          </li>
+          <li className="text-white self-center">Cart</li>
+          <li className="text-white self-center">Profile</li>
+        </ul>
       </div>
     </div>
   );
