b5eb8279fe5206052be6a76f91519905fa9ecff9
Michał Kiełbasa <70415923+aftermath321@users.noreply.github.com>
2023-11-01 21:28:04 +0100

    Added hovers and some minor config changes

--- src/main/js/components/Header.tsx
+++ src/main/js/components/Header.tsx
@@ -2,23 +2,29 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="absolute z-10 top-0 w-full bg-black/10 h-[100px] flex flex-row justify-between p-4 font-medium border-white/10 border-b-2 border-solid">
+    <div className="absolute z-10 top-0 w-full bg-black/10 h-[100px] flex flex-row justify-between p-4 border-white/10 border-b-2 border-solid font-semibold">
       {/* Left */}
       <div className="left-0 flex flex-row">
-        <div className="p-4 font-extrabold text-white text-4xl cursor-pointer">
-          H
-        </div>
-        <div className="p-4 font-extrabold text-white text-2xl cursor-pointer">
-          HomeOFFFICE
+        <div className="group flex flex-row">
+          <div className="p-4 font-extrabold text-white text-4xl cursor-pointer group-hover:scale-110 duration-200">
+            H
+          </div>
+          <div className="p-4 font-extrabold text-white text-2xl cursor-pointer group-hover:text-yellow-500 duration-200">
+            HomeOFFFICE
+          </div>
         </div>
         <div className="p-4 flex">
           <ul className="flex flex-row gap-4">
-            <li className="text-white self-center cursor-pointer ">
+            <li className="text-white self-center cursor-pointer  hover:text-yellow-500 duration-200">
               Our locations
             </li>
-            <li className="text-white self-center cursor-pointer">Store</li>
-            <li className="text-white self-center cursor-pointer">Office</li>
-            <li className="text-white self-center cursor-pointer">
+            <li className="text-white self-center cursor-pointer hover:text-yellow-500 duration-200">
+              Store
+            </li>
+            <li className="text-white self-center cursor-pointer hover:text-yellow-500 duration-200">
+              Office
+            </li>
+            <li className="text-white self-center cursor-pointer hover:text-yellow-500 duration-200">
               Home Office
             </li>
           </ul>
@@ -28,21 +34,29 @@ const Header = () => {
       {/* Right */}
       <div className="right-0 flex flex-row">
         <ul className="flex flex-row gap-4">
-          <li className="text-white self-center cursor-pointer">About</li>
-          <li className="text-white self-center cursor-pointer">Contact Us</li>
-          <li className="text-white self-center">
+          <li className="text-white self-center cursor-pointer hover:text-yellow-500 duration-200">
+            About
+          </li>
+          <li className="text-white self-center cursor-pointer hover:text-yellow-500 duration-200">
+            Contact Us
+          </li>
+          <li className="text-white self-center hover:text-yellow-500 duration-200">
             <div className="flex gap-2">
               <input
                 placeholder="Search products..."
                 className="p-1 rounded-md cursor-pointer px-2"
               ></input>
-              <button className="bg-yellow-300 px-3 rounded-md text-black font-bold cursor-pointer border border-black shadow">
+              <button className="bg-yellow-300 hover:bg-yellow-500 duration-200 px-3 rounded-md text-black font-bold cursor-pointer border border-black shadow">
                 &gt;
               </button>
             </div>
           </li>
-          <li className="text-white self-center cursor-pointer">Cart</li>
-          <li className="text-white self-center cursor-pointer">Profile</li>
+          <li className="text-white self-center cursor-pointer hover:text-yellow-500 duration-200">
+            Cart
+          </li>
+          <li className="text-white self-center cursor-pointer hover:text-yellow-500 duration-200">
+            Profile
+          </li>
         </ul>
       </div>
     </div>
--- src/main/js/components/Main.tsx
+++ src/main/js/components/Main.tsx
@@ -3,9 +3,8 @@ import Header from "./Header";
 
 const Main = () => {
   return (
-    <div className="relative w-full h-screen p-0 m-0">
+    <div className="relative w-full h-screen p-0 m-0 font-">
       <Header />
-      {/* <div className="backdrop-brightness-75 bg-black w-[100%] h-[100%]"> */}
       <div
         className="w-[100%] h-[100%] flex flex-col justify-center
         items-center relative overflow-hidden"
@@ -14,13 +13,12 @@ const Main = () => {
           className="inset-0 absolute w-full h-full brightness-75 object-cover"
           src="./src/main/static/public/homeOF.jpg"
         />
-        <div className=" w-[70%] h-[70vh] flex flex-col justify-center gap-10 relative top-[-10%]">
-          <p className="text-white text-2xl ">Office & Home Office furniture</p>
+        <div className=" w-[70%] h-[70vh] flex flex-col justify-center gap-10 relative text-shadow-sm">
+          <p className="text-white text-2xl font-semibold ">Office & Home Office furniture</p>
           <p className="text-white font-bold text-6xl text-shadow-lg">
             Improve workplace, improve results - <br /> all on a budget!
           </p>
-          {/* bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow */}
-          <button className="bg-yellow-300 p-2 px-10 w-[250px] rounded-md text-black font-bold cursor-pointer border border-black shadow">
+          <button className="bg-yellow-300 p-2 px-10 w-[250px] hover:bg-yellow-500 duration-200 rounded-md text-black font-bold cursor-pointer border border-black shadow">
             Visit Store
           </button>
         </div>
