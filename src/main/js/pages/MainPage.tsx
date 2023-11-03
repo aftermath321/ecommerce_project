36c95ab6132206f1b70a7b2b78ead7903cce923c
Michał Kiełbasa <70415923+aftermath321@users.noreply.github.com>
2023-11-03 20:03:29 +0100

    Added shop features

--- src/main/js/pages/MainPage.tsx
+++ src/main/js/pages/MainPage.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "../components/Header";
 import Main from "../components/Main";
 import Footer from "../components/Footer";
+import Features from "../components/Features"
 
 function MainPage() {
   return (
     <>
       <Header />
       <Main />
+      <Features />
       <Footer/>
     </>
   );
