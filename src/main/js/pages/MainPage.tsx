79e1f7d4f71bb2a0da044d83428330c03044bdba
Michał Kiełbasa <70415923+aftermath321@users.noreply.github.com>
2023-11-06 20:26:14 +0100

    Added mobile menu

--- src/main/js/pages/MainPage.tsx
+++ src/main/js/pages/MainPage.tsx
@@ -1,18 +1,20 @@
-import React from "react";
 import Header from "../components/Header";
 import Main from "../components/Main";
 import Footer from "../components/Footer";
-import Features from "../components/Features"
+import Features from "../components/Features";
 import ProductGrid from "../components/ProductGrid";
+import { useState } from "react";
 
 function MainPage() {
+  const [mobile, setMobile] = useState<boolean>(false);
+
   return (
     <>
-      <Header />
+      <Header mobileMenu={mobile} mobileMenuSetter={setMobile} />
       <Main />
       <ProductGrid />
       <Features />
-      <Footer/>
+      <Footer />
     </>
   );
 }
