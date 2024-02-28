7275c076dd94cb48a0270c73167f4a02d2f50648
Michał Kiełbasa <70415923+aftermath321@users.noreply.github.com>
2024-02-27 20:15:13 -0500

    Back to project, adding addresses

---
+++ src/main/js/components/Admin/Main.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div className="w-[100%] h-[100vh] ">
+      <div className="w-[80%] h-[100%] bg-gray-400 left-0 right-0 mx-auto">
+        <ul className="left-0 right-0 mx-auto bg-gray-800 h-[100%] w-[100%] m-2 p-2">
+          <li>Add item</li>
+          <li>Change text</li>
+          <li>Change picture</li>
+          <li>Manage users</li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Main
