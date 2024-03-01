7c6430a9f673bb4d271859134440edd6636240b2
Michał Kiełbasa <70415923+aftermath321@users.noreply.github.com>
2024-02-29 22:32:16 -0500

    Begininng of addproduct page and logic

--- src/main/js/components/Admin/AdminContent.tsx
+++ src/main/js/components/Admin/AdminContent.tsx
@@ -1,20 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const AdminContent = () => {
   return (
     <div className="w-[100%] h-[100vh] ">
-      <div className="w-[80%] h-[100%] bg-gray-400 left-0 right-0 mx-auto">
-        <ul className="left-0 right-0 mx-auto bg-gray-200 h-[100%] w-[100%] m-2 p-2 text-4xl flex flex-col align-center justify-center text-center gap-y-4">
-          <li className='hover:text-yellow-400 duration-300'>Add item</li>
-          <li className='hover:text-yellow-400 duration-300'>Change text</li>
-          <li className='hover:text-yellow-400 duration-300'>Change picture</li>
-          <li className='hover:text-yellow-400 duration-300'>Manage users</li>
+      <div className="w-[80%] h-[100%] bg-white left-0 right-0 mx-auto">
+        <ul className="left-0 right-0 mx-auto bg-white h-[100%] w-[100%] m-2 p-2 text-4xl flex flex-col align-center justify-center text-center gap-y-4">
+          <Link to={"/admin/addproduct"}>
+            <li className="hover:text-yellow-400 duration-300">Add item</li>
+          </Link>
+          <li className="hover:text-yellow-400 duration-300">Change text</li>
+          <li className="hover:text-yellow-400 duration-300">Change picture</li>
+          <li className="hover:text-yellow-400 duration-300">Manage users</li>
           <li></li>
           <li></li>
         </ul>
       </div>
     </div>
   );
-}
+};
 
 export default AdminContent;
--- src/main/js/components/Product/AddProduct.tsx
+++ src/main/js/components/Product/AddProduct.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import { Form } from "react-router-dom";
 
 const AddProduct = () => {
   return (
-    <></>
-  )
-}
+    <div className="w-[100%] h-[100vh] ">
+      <div className="w-[80%] h-[100%] bg-gray-200 left-0 right-0 mx-auto">
+        <form className="">
+          <button type="submit" />
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default AddProduct
+export default AddProduct;
