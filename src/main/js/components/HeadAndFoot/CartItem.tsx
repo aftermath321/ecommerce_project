dbd1592decba71955b2f6bb7fc9fafa42c0b3027
Michał Kiełbasa <70415923+aftermath321@users.noreply.github.com>
2024-03-04 00:04:18 -0500

    Upgraded cart

--- src/main/js/components/HeadAndFoot/CartItem.tsx
+++ src/main/js/components/HeadAndFoot/CartItem.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Product } from "../../../types/Product";
 
-const CartItem = (props: {id: number}) => {
+const CartItem = (props: { product: Product | undefined }) => {
   return (
-    <div>CartItem</div>
-  )
-}
+    <div className="w-[100%] h-[150px]  flex flex-row">
+      <div>
+        <img
+          className="inset-0 w-[150px] h-[150px] brightness-75 object-cover rounded-lg scale-90"
+          src={props?.product?.imagePath}
+        />
+      </div>
+      <div className="w-[450px] h-[100%] flex flex-row  justify-between">
+        <div className="align-middle flex flex-col self-center">
+          <p className="text-3xl ">{props.product?.name}</p>
+          <p className="text-xl text-gray-500">{props.product?.producer}</p>
+        </div>
+        <div className="text-2xl self-center">{props.product?.price}</div>
+      </div>
+    </div>
+  );
+};
 
-export default CartItem
+export default CartItem;
--- src/main/js/components/HeadAndFoot/CartList.tsx
+++ src/main/js/components/HeadAndFoot/CartList.tsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React from "react";
+import CartItem from "./CartItem";
+import { Product } from "../../../types/Product";
 
-const CartList = () => {
+const CartList = (props: {product:Product | undefined}) => {
   return (
-    <>
-        
-    
-    </>
-  )
-}
+    <div className="flex flex-col gap-2">
+      <CartItem product={props.product} />
+      <div className="w-[100%] h-[1px] bg-black"></div>
+      <CartItem product={props.product} />
+      <div className="w-[100%] h-[1px] bg-black"></div>
+      <CartItem product={props.product} />
+      <div className="w-[100%] h-[1px] bg-black"></div>
+      <div className="flex flex-row justify-between py-4">
+        <p className="text-3xl">Total:</p>
+        <p className="text-4xl border-black border-b-[1px]">90.15</p>
+      </div>
+    </div>
+  );
+};
 
-export default CartList
+export default CartList;
