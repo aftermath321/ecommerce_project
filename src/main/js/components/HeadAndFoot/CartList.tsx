6f20ce33def576094b8df1a220ada1e28ecebfc8
Michał Kiełbasa <70415923+aftermath321@users.noreply.github.com>
2024-03-04 18:31:09 -0500

    Fighting with the cartlist

--- src/main/js/components/HeadAndFoot/CartList.tsx
+++ src/main/js/components/HeadAndFoot/CartList.tsx
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { ReactNode, useState } from "react";
 import CartItem from "./CartItem";
 import { Product } from "../../../types/Product";
 
-const CartList = (props: {product:Product | undefined}) => {
+const CartList = (props: { product: Product[]}) => {
+  const [price, setPrice] = useState<number>(0.0)
+  console.log(props.product)
+
+
+  const calcPrice = ():void => {
+    props.product.forEach((element) => {
+      setPrice(element.price)
+      
+    });
+
+  };
+
+  const listItems = () => {
+    
+     for (let i = 0; i < props.product.length; i++){
+        return (
+
+          <>
+          <CartItem product={props.product[i]} />
+          <div className="w-[100%] h-[1px] bg-black"></div>
+          </>
+
+        )
+    }
+  }
+
+
   return (
     <div className="flex flex-col gap-2">
-      <CartItem product={props.product} />
-      <div className="w-[100%] h-[1px] bg-black"></div>
-      <CartItem product={props.product} />
-      <div className="w-[100%] h-[1px] bg-black"></div>
-      <CartItem product={props.product} />
-      <div className="w-[100%] h-[1px] bg-black"></div>
+      {listItems()}
       <div className="flex flex-row justify-between py-4">
         <p className="text-3xl">Total:</p>
-        <p className="text-4xl border-black border-b-[1px]">90.15</p>
+        <p className="text-4xl border-black border-b-[1px]">{price}</p>
       </div>
     </div>
   );
