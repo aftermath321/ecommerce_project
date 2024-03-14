59e2386315bd5e458e8720d87fec4d1fbf36a9ec
Michał Kiełbasa <70415923+aftermath321@users.noreply.github.com>
2024-03-13 20:44:37 -0400

    Small changes

--- src/main/js/components/Product/AddProduct.tsx
+++ src/main/js/components/Product/AddProduct.tsx
@@ -1,48 +1,39 @@
-// type ProductTemp = {
-//   name: "";
-//   price: number;
-//   producer: "";
-//   imagePath: "";
-//   rating: number;
-// };
-
 const AddProduct = () => {
-
   return (
     <div className="w-[100%] h-[100vh] ">
       <div className="w-[80%] h-[100%] bg-gray-200 left-0 right-0 mx-auto">
-        <form className="flex flex-col w-[80%] h-[100%] gap-4 bg-white mx-auto left-0 right-0 justify-center text-black text-lg align-middle text-center">
+        <form className="flex flex-col w-[80%] h-[100%] gap-4 bg-white mx-auto left-0 right-0 justify-center text-black text-lg align-middle text-center" method="POST">
           <label>Name</label>
           <input
             type="text"
-            className=" shadow-bigShadow w-[400px] h-[50px] bg-slate-200 rounded-lg self-center "
+            className=" shadow-bigShadow w-[400px] h-[50px] bg-slate-200 rounded-lg self-center p-2"
           ></input>
           <label>Price</label>
           <input
             type="text"
-            className="shadow-bigShadow w-[400px] h-[50px] bg-slate-200 rounded-lg  self-center "
+            className="shadow-bigShadow w-[400px] h-[50px] bg-slate-200 rounded-lg  self-center p-2"
           ></input>
           <label>Producer</label>
           <input
             type="text"
-            className="shadow-bigShadow w-[400px] h-[50px] bg-slate-200 rounded-lg  self-center "
+            className="shadow-bigShadow w-[400px] h-[50px] bg-slate-200 rounded-lg  self-center p-2"
           ></input>
           <label>Image Path</label>
           <input
             type="text"
-            className="shadow-bigShadow w-[400px] h-[50px] bg-slate-200 rounded-lg  self-center "
+            className="shadow-bigShadow w-[400px] h-[50px] bg-slate-200 rounded-lg  self-center p-2"
           ></input>
           <label>Rating</label>
           <input
             type="text"
-            className="shadow-bigShadow w-[400px] h-[50px] bg-slate-200 rounded-lg  self-center "
+            className="shadow-bigShadow w-[400px] h-[50px] bg-slate-200 rounded-lg  self-center p-2"
           ></input>
 
           <button
             type="submit"
             className="w-[200px] h-[75px] shadow-button rounded-lg p-4 px-8 bg-yellow-300 hover:bg-yellow-400 duration-300 mx-auto left-0 right-0"
           >
-            Add Product{" "}
+            Add Product
           </button>
         </form>
       </div>
--- src/main/js/components/Product/SearchResult.tsx
+++ src/main/js/components/Product/SearchResult.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Product } from "../../../types/Product";
 import { searchProductByName } from "../../api/productsAPI";
 import { Link } from "react-router-dom";
