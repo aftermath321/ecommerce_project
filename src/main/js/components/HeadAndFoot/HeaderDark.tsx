dbd5b8c6b153f43000bb114c1c4f4ad9ec4e48f3
Michał Kiełbasa <70415923+aftermath321@users.noreply.github.com>
2024-03-17 21:36:27 -0400

    MobileMenu improvement and link fixed

--- src/main/js/components/HeadAndFoot/HeaderDark.tsx
+++ src/main/js/components/HeadAndFoot/HeaderDark.tsx
@@ -1,52 +1,23 @@
 import { GiHamburgerMenu } from "react-icons/gi";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { BsFillPersonFill } from "react-icons/bs";
-import { RxCross1 } from "react-icons/rx";
 import { Link } from "react-router-dom";
-import { useState } from "react";
-import CartList from "../CartMenu/CartList";
+import { SetStateAction, useState } from "react";
 import CartMenu from "../CartMenu/CartMenu";
+import MobileMenu from "../Mobile/MobileMenu";
 
 
 const HeaderDark = (props: {
   mobileMenu: boolean;
-  mobileMenuSetter: Function;
+  mobileMenuSetter: React.Dispatch<SetStateAction<boolean>>;
 }) => {
   const [profileState] = useState<boolean>(true);
   const [cartMenu, setCartMenu] = useState<boolean>(false);
   const [search, setSearch] = useState<string>("");
 
-
-
   const mobileMenuDark = (): JSX.Element => {
     if (props.mobileMenu) {
-      return (
-        <div className="w-full h-[100vh] fixed top-0 left-0 bg-black/75 z-50">
-          <span
-            className="absolute text-white z-60 top-[5%] right-[10%]"
-            onClick={() => props.mobileMenuSetter(false)}
-          >
-            <RxCross1 size={40} />
-          </span>
-          <h1 className="text-white text-5xl absolute left-[10%] top-[5%]">
-            H
-          </h1>
-          <ul className="w-[90%] h-[80%] font-bold gap-4 text-white text-3xl justify-center flex flex-col items-center">
-            <li className="active:text-yellow-400">
-              <Link to="/sign-up">Sign up</Link>
-            </li>
-            <li className="active:text-yellow-400">Login</li>
-            <li className="active:text-yellow-400">Locations</li>
-            <li className="active:text-yellow-400">Shop</li>
-            <li className="active:text-yellow-400">Office</li>
-            <li className="active:text-yellow-400">Home Office</li>
-            <li className="active:text-yellow-400">
-              <Link to="/about-us">About us</Link>
-            </li>
-            <li className="active:text-yellow-400">Contact</li>
-          </ul>
-        </div>
-      );
+      return <MobileMenu menuToggle={props.mobileMenuSetter} />;
     } else {
       return <></>;
     }
@@ -64,9 +35,11 @@ const HeaderDark = (props: {
     }
   };
   const cartMenuToggle = (): JSX.Element => {
-    return (<>
-      <CartMenu toggleMenu={setCartMenu}/>
-      </>)
+    return (
+      <>
+        <CartMenu toggleMenu={setCartMenu} />
+      </>
+    );
   };
 
   return (
--- src/main/js/components/HeadAndFoot/HeaderLight.tsx
+++ src/main/js/components/HeadAndFoot/HeaderLight.tsx
@@ -3,47 +3,21 @@ import { AiOutlineShoppingCart } from "react-icons/ai";
 import { BsFillPersonFill } from "react-icons/bs";
 import { RxCross1 } from "react-icons/rx";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { SetStateAction, useState } from "react";
 import CartMenu from "../CartMenu/CartMenu";
+import MobileMenu from "../Mobile/MobileMenu";
 
 const HeaderLight = (props: {
   mobileMenu: boolean;
-  mobileMenuSetter: Function;
+  mobileMenuSetter: React.Dispatch<SetStateAction<boolean>>;
 }) => {
   const [profileState] = useState<boolean>(true);
   const [cartMenu, setCartMenu] = useState<boolean>(false);
   const [search, setSearch] = useState<string>("");
 
-
   const mobileMenuDark = (): JSX.Element => {
     if (props.mobileMenu) {
-      return (
-        <div className="w-full h-[100vh] fixed top-0 left-0 bg-black/75 z-50">
-          <span
-            className="absolute text-black z-60 top-[5%] right-[10%]"
-            onClick={() => props.mobileMenuSetter(false)}
-          >
-            <RxCross1 size={40} />
-          </span>
-          <h1 className="text-black text-5xl absolute left-[10%] top-[5%]">
-            H
-          </h1>
-          <ul className="w-[90%] h-[80%] font-bold gap-4 text-black text-3xl justify-center flex flex-col items-center">
-            <li className="active:text-yellow-400">
-              <Link to="/sign-up">Sign up</Link>
-            </li>
-            <li className="active:text-yellow-400">Login</li>
-            <li className="active:text-yellow-400">Locations</li>
-            <li className="active:text-yellow-400">Shop</li>
-            <li className="active:text-yellow-400">Office</li>
-            <li className="active:text-yellow-400">Home Office</li>
-            <li className="active:text-yellow-400">
-              <Link to="/about-us">About us</Link>
-            </li>
-            <li className="active:text-yellow-400">Contact</li>
-          </ul>
-        </div>
-      );
+      return <MobileMenu menuToggle={props.mobileMenuSetter} />;
     } else {
       return <></>;
     }
@@ -61,11 +35,11 @@ const HeaderLight = (props: {
     }
   };
   const cartMenuToggle = (): JSX.Element => {
-   return (
-     <>
-       <CartMenu toggleMenu={setCartMenu} />
-     </>
-   );
+    return (
+      <>
+        <CartMenu toggleMenu={setCartMenu} />
+      </>
+    );
   };
 
   return (
@@ -75,10 +49,10 @@ const HeaderLight = (props: {
       {/* Mobile Display */}
       <div className="flex flex-row md:hidden bg-white border-white/10 border-b-2 border-solid absolute z-10 top-0 w-full h-[75px]">
         <div className="p-4 font-extrabold text-black text-3xl cursor-pointer group-hover:scale-110 duration-200">
-          H
+          <Link to="/">H</Link>
         </div>
         <div className="p-4 py-6 font-extrabold text-black text-xl cursor-pointer group-hover:text-yellow-500 duration-200">
-          HomeOFFFICE
+          <Link to="/"> HomeOFFFICE</Link>
         </div>
         <div className="flex flex-row p-4 absolute right-0 ">
           <span className="text-black p-2" onClick={() => setCartMenu(true)}>
@@ -99,7 +73,7 @@ const HeaderLight = (props: {
         <div className="left-0 flex flex-row">
           <div className="group flex flex-row">
             <div className="p-4 font-extrabold text-black text-4xl cursor-pointer group-hover:scale-110 duration-200">
-              H
+              <Link to="/">H</Link>
             </div>
             <div className="p-4 font-extrabold text-black text-2xl cursor-pointer group-hover:text-yellow-500 duration-200">
               <Link to="/"> HomeOFFFICE</Link>
