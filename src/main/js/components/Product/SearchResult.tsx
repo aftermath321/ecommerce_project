a88f40dee5ddbfad9063b7a7f1842a49ac977ee8
Michał Kiełbasa <77aftermath77@gmail.com>
2024-03-21 20:15:54 -0400

    Fixed search

--- src/main/js/components/Product/SearchResult.tsx
+++ src/main/js/components/Product/SearchResult.tsx
@@ -6,7 +6,7 @@ import ProductCard from "./ProductCard";
 
 const SearchResult = (props: { phrase: string }) => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [productList, setProductList] = useState<Product[]>([]);
+  const [productList, setProductList] = useState<Product[] | null>([]);
 
   useEffect(() => {
     setIsLoading(true);
@@ -20,21 +20,31 @@ const SearchResult = (props: { phrase: string }) => {
     if (isLoading) {
       return <>Loading</>;
     } else {
-      return (
-        <div className="w-[100%] h-[90%] flex justify-center flex-wrap  text-center p-4  gap-y-8 gap-x-32">
-          <ul className="w-[100%] h-[90%] flex justify-center flex-wrap  text-center p-4  gap-y-8 gap-x-32  bg-white">
-            {productList.map((item) => {
-              return (
-                <li key={item.id}>
-                  <Link to={"/product/" + item.id}>
-                    <ProductCard product={item} />
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      );
+      if (productList?.length == 0) {
+        return (
+          <div className="w-[100%] h-[90%] flex justify-center flex-wrap  text-center p-4  gap-y-8 gap-x-32">
+            <div className="w-[100%] h-[90%] flex justify-center flex-wrap  text-center p-4  gap-y-8 gap-x-32  bg-white">
+              <h1 className="text-3xl self-center">No such products</h1>
+            </div>
+          </div>
+        );
+      } else {
+        return (
+          <div className="w-[100%] h-[90%] flex justify-center flex-wrap  text-center p-4  gap-y-8 gap-x-32">
+            <ul className="w-[100%] h-[90%] flex justify-center flex-wrap  text-center p-4  gap-y-8 gap-x-32  bg-white">
+              {productList?.map((item) => {
+                return (
+                  <li key={item.id}>
+                    <Link to={"/product/" + item.id}>
+                      <ProductCard product={item} />
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        );
+      }
     }
   };
 
