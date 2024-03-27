ce8f05732bc999fdbdb0fa622d13a82704b5b77b
Michał Kiełbasa <77aftermath77@gmail.com>
2024-03-26 23:12:53 -0400

    Security pt. 3

--- src/main/java/com/ecp/ecommerceproject/services/UserDetailService.java
+++ src/main/java/com/ecp/ecommerceproject/services/UserDetailService.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 import org.springframework.security.core.userdetails.User;
 
 
 import com.ecp.ecommerceproject.model.MyUser;
 import com.ecp.ecommerceproject.repositories.UserRepo;
 
+@Service
 public class UserDetailService implements UserDetailsService{
 
 
