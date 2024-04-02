195c7125667bfa335b0bfd7395bca06be07707cf
Michał Kiełbasa <77aftermath77@gmail.com>
2024-04-01 22:46:50 -0400

    Changes vol. 2

--- src/main/js/components/SiteComponents/Banner.tsx
+++ src/main/js/components/SiteComponents/Banner.tsx
@@ -3,13 +3,13 @@ import React from "react";
 const Banner = () => {
   return (
     <div className="w-[100%] h-[60vh] h-max-[700px] relative ">
-      <div className="flex relative z-10 flex-col gap-4 h-[80%] justify-center items-center ">
+      <div className="flex relative z-10 flex-col gap-6 h-[80%] justify-center items-center ">
         <h1 className="text-xl text-white p-2">Our recommendation</h1>
-        <h1 className="text-5xl text-white p-4">
+        <h1 className="text-8xl font-bold text-white p-6">
           Browse the newest collection!
         </h1>
-        <button className="bg-yellow-400 md:px-10  p-4 md:p-2 w-[150px] md:w-[250px] hover:bg-yellow-600 duration-200 rounded-md text-black font-bold cursor-pointer shadow-button">
-          Visit Store
+        <button className="bg-yellow-400 md:px-10  p-6 md:p-2 w-[150px] md:w-[250px] hover:bg-yellow-600 duration-200 rounded-md text-black font-light text-xl cursor-pointer shadow-button">
+          Explore
         </button>
       </div>
       <img
